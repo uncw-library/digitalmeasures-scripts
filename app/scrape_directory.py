6f3d7d74db96e7b45b369806875eaab6164f2d04
Garrett Armstrong <armstrongg@uncw.edu>
2022-12-13 12:32:06 -0500

    added selenium exception

--- app/scrape_directory.py
+++ app/scrape_directory.py
@@ -3,6 +3,7 @@
 import os
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
